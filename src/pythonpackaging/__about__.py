7d80985e5069fd209309b4a4c261353047f06a3c
Your Name <you@example.com>
2019-12-10 15:01:23 -0800

    code for packaging

---
+++ src/pythonpackaging/__about__.py
@@ -0,0 +1,20 @@
+# __all__ = [
+#     "__title__", "__summary__", "__uri__", "__version__", "__author__",
+#     "__email__", "__license__", "__copyright__",
+# ]
+__all__ = [
+    "__title__", "__summary__", "__uri__", "__version__", "__author__",
+    "__email__",
+]
+
+__title__ = "pythonpackaging"
+__summary__ = "Deployment example package"
+__uri__ = ""
+
+__version__ = "1.0.0"
+
+__author__ = "Anshu Kumar"
+__email__ = "anshuk@uw.edu"
+
+# __license__ = "GNU GPLv3"
+# __copyright__ = f"Copyright 2016-2019 {__author__}"
